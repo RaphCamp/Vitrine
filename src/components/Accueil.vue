1fdd9ce7fe47a2dbf87df3c442012e2d1ae0dab2
Raphael Campagnac <rcampagnac@cegid.com>
2022-08-16 12:25:26 +0200

    Changement icone, ajout navbar et accueil

---
+++ src/components/Accueil.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="accueil">
+  <div id="banner">
+  <h1>Raph le Bricodeur</h1>
+  </div>
+  <div id="illustration"></div>
+  <div id="presentation"></div>
+  </div>
+</template>
+
+
+<style scoped>
+#banner {
+  background-color: rgb(187, 181, 100);
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  border-bottom: 2px solid black;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+</script>
+
+<template>
+    <div class="nav">
+        <div class="buttons">
+            <p id="accueil">Accueil</p>
+            <p>Mon CV</p>
+            <p>Projets Persos</p>
+        </div>
+        <div id="else"></div>
+    </div>
+</template>
+
+<style scoped>
+.nav {
+    display: flex;
+    background-color: rgb(179, 19, 19);
+    width: 100%;
+    height: 50px;
+    margin: 0;
+    border-bottom: 5px solid rgb(66, 60, 60);
+}
+.buttons {
+    flex: 1;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 10px;
+}
+
+.buttons p{
+    color: aliceblue;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    font-size: 1.5em;
+}
+#accueil {
+    font-size: 2.6em;
+}
+#else {
+    flex: 3;
+}
+</style>
